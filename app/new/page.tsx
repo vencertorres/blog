a5bededec8419f817126ef905a923d7e7b98fb99
Vencer Torres <vencer.torres@gmail.com>
2023-05-26 15:41:03 +0800

    feat: add create post form

---
+++ app/new/page.tsx
@@ -0,0 +1,68 @@
+import { sql } from "@vercel/postgres";
+import { redirect } from "next/navigation";
+import slugify from "slugify";
+
+export default async function CreatePost() {
+  async function publish(data: FormData) {
+    "use server";
+
+    const title = data.get("title") as string;
+    const body = data.get("body") as string;
+    const slug = slugify(`${title} ${Math.random().toString(36).slice(2, 7)}`, {
+      lower: true,
+    });
+
+    await sql`
+          INSERT INTO posts(title, body, slug)
+          VALUES (${title}, ${body}, ${slug});
+          `;
+
+    redirect(`/posts/${slug}`);
+  }
+
+  async function cancel() {
+    "use server";
+    redirect("/");
+  }
+
+  return (
+    <div className="mx-auto max-w-[42rem] py-24 sm:py-32">
+      <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">
+        Create post
+      </h1>
+
+      <div className="mt-10">
+        <form action={publish} className="space-y-6">
+          <div className="form-control">
+            <label htmlFor="title" className="label">
+              <span className="label-text">Title</span>
+            </label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              className="input-bordered input"
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="body" className="label">
+              <span className="label-text">Body</span>
+            </label>
+            <textarea
+              name="body"
+              id="body"
+              rows={5}
+              className="textarea-bordered textarea"
+            ></textarea>
+          </div>
+
+          <button className="btn">Publish</button>
+          <button className="btn-link btn" formAction={cancel}>
+            Cancel
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
