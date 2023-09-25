f51b314996c42ac38cbc0b5f95ce3f416fae6bd4
Vencer Torres <vencer.torres@gmail.com>
2023-09-25 11:54:10 +0800

    fix nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,7 +6,7 @@ export default async function Nav() {
 	const user = await getCurrentUser();
 
 	return (
-		<nav className="fixed top-0 w-screen border-b bg-white px-6">
+		<nav className="fixed top-0 z-10 w-screen border-b bg-white px-6">
 			<div className="container mx-auto flex h-16 items-center justify-between">
 				<Link href="/">Blog.</Link>
 				{user ? <UserNav user={user} /> : <Link href="/login">Log in</Link>}
