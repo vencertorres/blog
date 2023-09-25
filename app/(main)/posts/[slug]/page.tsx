f110fe2ddf3ac0de831971d9ad78164dcbdb1bd8
Vencer Torres <vencer.torres@gmail.com>
2023-09-25 11:15:48 +0800

    add author and date to post page

--- app/(main)/posts/[slug]/page.tsx
+++ app/(main)/posts/[slug]/page.tsx
@@ -1,11 +1,16 @@
 import prisma from '@/lib/db';
+import { format, formatISO9075 } from 'date-fns';
 import Blocks from 'editorjs-blocks-react-renderer';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 async function getPost(slug: string) {
 	return await prisma.post.findUnique({
 		where: {
 			slug: slug,
+		},
+		include: {
+			author: true,
 		},
 	});
 }
@@ -30,13 +35,27 @@ export default async function Post({ params }: { params: { slug: string } }) {
 	}
 
 	return (
-		<main className="flex-1 bg-white">
-			<article className="prose mx-auto px-6 py-12">
-				<h1>{post.title}</h1>
-				<p>{post.description}</p>
-				{/* @ts-ignore */}
-				{post.content && <Blocks data={post.content} />}
-			</article>
-		</main>
+		<article className="prose mx-auto">
+			<div className="not-prose mb-2 border-b-2 pb-4">
+				<h1 className="text-4xl font-bold text-gray-900">{post.title}</h1>
+				<p className="text-gray-500">{post.description}</p>
+			</div>
+			<div className="not-prose text-xs font-medium">
+				<Link
+					href={`/profile/@${post.author.username}`}
+					className="text-sky-500 underline hover:decoration-2"
+				>
+					{post.author.name}
+				</Link>{' '}
+				<time
+					dateTime={formatISO9075(post.createdAt, { representation: 'date' })}
+					className="text-gray-500"
+				>
+					{format(post.createdAt, 'LLL d, yyyy')}
+				</time>
+			</div>
+			{/* @ts-ignore */}
+			{post.content && <Blocks data={post.content} />}
+		</article>
 	);
 }
