94021e83d9d8a90a6755af8e148d94e345ed4151
Vencer Torres <vencer.torres@gmail.com>
2023-09-25 11:06:37 +0800

    make nav fixed

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,10 @@
-import MainNav from '@/components/MainNav';
+import Nav from '@/components/Nav';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="flex min-h-screen flex-col">
-			<MainNav />
-			<main className="flex flex-1 flex-col px-6 py-12">{children}</main>
+		<div className="min-h-screen">
+			<Nav />
+			<main className="mt-16 flex flex-1 flex-col px-6 py-12">{children}</main>
 		</div>
 	);
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,11 +2,11 @@ import { getCurrentUser } from '@/lib/auth';
 import Link from 'next/link';
 import UserNav from './UserNav';
 
-export default async function MainNav() {
+export default async function Nav() {
 	const user = await getCurrentUser();
 
 	return (
-		<nav className="border-b bg-white px-6">
+		<nav className="fixed top-0 w-screen border-b bg-white px-6">
 			<div className="container mx-auto flex h-16 items-center justify-between">
 				<Link href="/">Blog.</Link>
 				{user ? <UserNav user={user} /> : <Link href="/login">Log in</Link>}
